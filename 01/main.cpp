3fc5062a187a7b5ba1633851c291021c0f530400
Vov-etc <vladim.kha@ya.ru>
2021-03-25 23:47:00 +0300

    more unit-tests for makeAllocator()

--- 01/main.cpp
+++ 01/main.cpp
@@ -47,6 +47,18 @@ void BigDataTest() {
     assert(ptr != nullptr);
 }
 
+void BunchOfMakeTest() {
+    Allocator allctr;
+    allctr.makeAllocator(4);
+    size_t size_1 = allctr.get_maxSize();
+    char* ptr_1 = allctr.get_ptr();
+    allctr.makeAllocator(2);
+    size_t size_2 = allctr.get_maxSize();
+    char* ptr_2 = allctr.get_ptr();
+    assert(size_1 != size_2);
+    //assert(ptr_1 != ptr_2);      will not work, new allocation will be on the same position
+}
+
 int main() {
     DefaulTest();
     NullTest();
@@ -54,6 +66,7 @@ int main() {
     BunchOfAllocTest();
     ResetTest();
     BigDataTest();
+    BunchOfMakeTest();
     std::cout << "Success!" << std::endl;
     return 0;
 }
